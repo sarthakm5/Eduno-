a3e3ca4a591fbda7b03f8da9d78c1ac324072219
sarthakm5 <tsarthakmehta@gmail.com>
2025-05-07 11:18:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  build: {
+    outDir: 'build',
+    emptyOutDir: true 
+  }
 })
